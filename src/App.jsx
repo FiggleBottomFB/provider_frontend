2330596a28beded7270e352f540ebf9fa5d7ee0c
Elias <elias.moll38@gmail.com>
2026-01-26 11:50:24 +0100

    udate

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Link, useNavigate, useParams } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [i, ia] = useState(0)
   return (
     <div>
       HELLO WORLD!!
+      <button onClick={()=>ia(prev=> prev+1)}>
+        {i}
+      </button>
     </div>
   )
 }
