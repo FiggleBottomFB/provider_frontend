4a68bb5472cb5e99fdbf42007c53b249681c35b7
Elias <elias.moll38@gmail.com>
2026-01-27 13:10:32 +0100

    added test file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Testfetch from './Test'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link, useNavigate, useParams } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
   const [i, ia] = useState(0)
   return (
     <BrowserRouter>
+      <Testfetch/>
       HELLO WORLD!!
       <button onClick={()=>ia(prev=> prev+1)}>
         {i}
