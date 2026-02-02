fb2ea64001d32d83c2c0a2902d9d1e21761b6eb6
FiggleBottomFB <elias.moll38@gmail.com>
2026-02-02 14:25:36 +0100

    finished the wiki functionality, history and display options

--- src/Wiki/Wikipagehistory.jsx
+++ src/Wiki/Wikipagehistory.jsx
@@ -1,5 +1,37 @@
-function WikiPageHistory(){
-    return <div>WikiPageHistory</div>
+import { useEffect, useState } from "react"
+import { getWikiHistory, restoreWikiHistory, getPerson } from "../apicalls"
+import { useAuth } from "../Auth/Authcontext"
+
+function WikiPageHistory({WikiPage}){
+    const [WikiHistory, setWikiHistory] = useState([])
+    const {user} = useAuth()
+
+    const [createdBy, setCreatedBy] = useState([])
+
+    useEffect(()=>{
+        const fetchWikiHistory = async () =>{
+            const wikiHistory = getWikiHistory(user.token, WikiPage.id)
+            setWikiHistory(wikiHistory)   
+        }
+        fetchWikiHistory()
+    }, [])
+
+    return (
+        <div id="wiki-page-history-container">
+            {
+                WikiHistory.map((version, index)=>(
+                    <div key={index} id="wiki-page-version-container">
+                        {version.timestamp}
+                        {
+                            setCreatedBy(getPerson(user.token, version.id))
+                        }
+                        Redigerad av: {createdBy.name}
+                        <button id="restore-version-button" onClick={()=>{restoreWikiHistory(user.id, version.id)}}>Återställ version</button>
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default WikiPageHistory
