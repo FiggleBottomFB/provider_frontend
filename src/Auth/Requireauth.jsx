977a604d7498b5de945b41846eb5f643871e51e8
Simon Andersson <simon.andersson4@elev.ga.ntig.se>
2026-02-04 15:15:05 +0100

    made blog use a common error and loading handler

--- src/Auth/Requireauth.jsx
+++ src/Auth/Requireauth.jsx
@@ -5,8 +5,13 @@ export function RequireAuth({ children, roles = [] }) {
   const { user, loading } = useAuth();
   const location = useLocation();
 
-  if (loading) return <h2>Loading...</h2>; // wait for verification
-  if (!user || (roles.length > 0 && !roles.includes(user.role))) {
+  if (loading) return <h2>Loading...</h2>;
+
+  // Assign role and if user isent logged in treat them as guest
+  const role = user?.role || "guest";
+
+  // If this route has role requirements and user doesn't match
+  if (roles.length > 0 && !roles.includes(role)) {
     return <Navigate to="/Login" replace state={{ from: location }} />;
   }
 
